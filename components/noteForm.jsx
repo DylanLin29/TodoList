c1d84c29ccf50e272286b0e01abea71768d87f7f
Dylan Lin <46723393+DylanLin29@users.noreply.github.com>
2020-07-22 18:15:20 -0700

    update

--- components/noteForm.jsx
+++ components/noteForm.jsx
@@ -4,125 +4,131 @@ import Form from "../components/common/form";
 import { Button, Select } from "semantic-ui-react";
 
 class NoteForm extends Form {
-  state = {
-    data: {
-      title: "",
-      description: "",
-      importance: 3,
-      category: "Uncategorized",
-      createDate: 0,
-    },
-    errors: {},
-    selectImportance: {
-      1: false,
-      2: false,
-      3: false,
-    },
-  };
+	state = {
+		data: {
+			title: "",
+			description: "",
+			importance: 3,
+			category: "Uncategorized",
+			createDate: 0,
+			check: false,
+		},
+		errors: {},
+		selectImportance: {
+			1: false,
+			2: false,
+			3: false,
+		},
+	};
 
-  schema = {
-    title: Joi.string().required().label("Title"),
-    description: Joi.string().required().label("Description"),
-    importance: Joi.number().optional(),
-  };
+	schema = {
+		title: Joi.string().required().label("Title"),
+		description: Joi.string().required().label("Description"),
+		importance: Joi.number().optional(),
+	};
 
-  handleCancel = () => {
-    const selectImportance = {
-      1: false,
-      2: false,
-      3: false,
-    };
-    const data = {
-      title: "",
-      description: "",
-      importance: 3,
-      category: "Uncategorized",
-      createDate: 0,
-    };
-    this.setState({ errors: {}, selectImportance, data });
-    this.props.handleOpenNote();
-  };
+	handleCancel = () => {
+		const selectImportance = {
+			1: false,
+			2: false,
+			3: false,
+		};
+		const data = {
+			title: "",
+			description: "",
+			importance: 3,
+			category: "Uncategorized",
+			createDate: 0,
+			check: false,
+		};
+		this.setState({ errors: {}, selectImportance, data });
+		this.props.handleOpenNote();
+	};
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.handleCreate(this.state.data);
-    this.handleCancel();
-  };
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.handleCreate(this.state.data);
+		this.handleCancel();
+	};
 
-  handleClickImportance = (index) => {
-    const updateSelectImportance = { ...this.state.selectImportance };
-    const updateData = { ...this.state.data };
-    updateData.importance = index;
-    for (let i = 1; i <= 3; i++) {
-      updateSelectImportance[i] = false;
-    }
-    updateSelectImportance[index] = true;
-    this.setState({
-      selectImportance: updateSelectImportance,
-      data: updateData,
-    });
-  };
+	handleClickImportance = (index) => {
+		const updateSelectImportance = {
+			...this.state.selectImportance,
+		};
+		const updateData = { ...this.state.data };
+		updateData.importance = index;
+		for (let i = 1; i <= 3; i++) {
+			updateSelectImportance[i] = false;
+		}
+		updateSelectImportance[index] = true;
+		this.setState({
+			selectImportance: updateSelectImportance,
+			data: updateData,
+		});
+	};
 
-  handleSelect = (event, option) => {
-    const data = this.state.data;
-    data.category = option.value;
-    this.setState({ data });
-  };
+	handleSelect = (event, option) => {
+		const data = this.state.data;
+		data.category = option.value;
+		this.setState({ data });
+	};
 
-  render() {
-    const importanceLevel = [1, 2, 3];
-    const categoryOptions = [
-      { key: "Home", value: "Home", text: "Home" },
-      { key: "School", value: "School", text: "School" },
-      { key: "Work", value: "Work", text: "Work" },
-    ];
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p className="note-title">Todo</p>
-        <hr />
-        {this.renderInput("title", "Title", "login-label")}
-        {this.renderInput("description", "Description", "login-label")}
-        <p className="form-label">Importance Level</p>
-        <Button.Group className="importance-buttons-group">
-          {importanceLevel.map((index) => {
-            return (
-              <Button
-                onClick={() => this.handleClickImportance(index)}
-                key={index}
-                type="button"
-                className={
-                  this.state.selectImportance[index]
-                    ? "importance-button-select"
-                    : "importance-button"
-                }
-              >
-                {index}
-              </Button>
-            );
-          })}
-        </Button.Group>
-        <p className="form-label">Category</p>
-        <Select
-          options={categoryOptions}
-          onChange={this.handleSelect}
-          className="note-select"
-          placeholder="Select a Category"
-        />
-        <br />
-        <Button
-          color="red"
-          onClick={this.handleCancel}
-          className="note-button"
-          type="button"
-        >
-          Cancel
-        </Button>
-        <Button color="blue" className="note-button">
-          Create
-        </Button>
-      </form>
-    );
-  }
+	render() {
+		const importanceLevel = [1, 2, 3];
+		const categoryOptions = [
+			{ key: "Home", value: "Home", text: "Home" },
+			{ key: "School", value: "School", text: "School" },
+			{ key: "Work", value: "Work", text: "Work" },
+		];
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<p className="note-title">Todo</p>
+				<hr />
+				{this.renderInput("title", "Title", "login-label")}
+				{this.renderInput("description", "Description", "login-label")}
+				<p className="form-label">Importance Level</p>
+				<Button.Group className="importance-buttons-group">
+					{importanceLevel.map((index) => {
+						return (
+							<Button
+								onClick={() =>
+									this.handleClickImportance(index)
+								}
+								key={index}
+								type="button"
+								className={
+									this.state.selectImportance[index]
+										? "importance-button-select"
+										: "importance-button"
+								}
+							>
+								{index}
+							</Button>
+						);
+					})}
+				</Button.Group>
+				<p className="form-label">Category</p>
+				<Select
+					options={categoryOptions}
+					onChange={this.handleSelect}
+					className="note-select"
+					placeholder="Select a Category"
+				/>
+				<br />
+				<Button
+					color="red"
+					onClick={this.handleCancel}
+					className="note-button"
+					type="button"
+				>
+					Cancel
+				</Button>
+				<Button color="blue" className="note-button">
+					Create
+				</Button>
+			</form>
+		);
+	}
 }
 
 export default NoteForm;
--- components/registerForm.jsx
+++ components/registerForm.jsx
@@ -3,73 +3,84 @@ const axios = require("axios");
 import Form from "./common/form";
 import Router from "next/router";
 class RegisterForm extends Form {
-  state = {
-    data: {
-      username: "",
-      email: "",
-      password: "",
-    },
-    errors: {},
-    response: {
-      success: true,
-      message: "",
-    },
-  };
+	state = {
+		data: {
+			username: "",
+			email: "",
+			password: "",
+		},
+		errors: {},
+		response: {
+			success: true,
+			message: "",
+		},
+	};
 
-  schema = {
-    username: Joi.string().min(5).required().label("Username"),
-    email: Joi.string().email().min(5).required().label("Email"),
-    password: Joi.string().min(5).required().label("Password"),
-  };
+	schema = {
+		username: Joi.string().min(5).required().label("Username"),
+		email: Joi.string().email().min(5).required().label("Email"),
+		password: Joi.string().min(5).required().label("Password"),
+	};
 
-  doSubmit = async () => {
-    // Call the server
-    try {
-      await axios.post("/api/users", this.state.data);
-      Router.push("/");
-    } catch (err) {
-      this.setState({ response: err.response.data });
-    }
-  };
+	doSubmit = async () => {
+		// Call the server
+		try {
+			await axios.post("/api/users", this.state.data);
+			Router.push("/");
+		} catch (err) {
+			this.setState({ response: err.response.data });
+		}
+	};
 
-  render() {
-    const { success, message } = this.state.response;
-    return (
-      <div className="container appForm">
-        <div className="">
-          <div className="card-section border rounded p-3">
-            <div className="card-header-register pb-5">
-              <h2 className="card-header-title text-white pt-3">Register</h2>
-            </div>
-            <div className="card-body">
-              <form onSubmit={this.handleSubmit}>
-                {this.renderInput("username", "Username", "register-label")}
-                {this.renderInput(
-                  "email",
-                  "Email",
-                  "register-label",
-                  undefined,
-                  success ? undefined : message
-                )}
-                {this.renderInput(
-                  "password",
-                  "Password",
-                  "register-label",
-                  "password"
-                )}
-                {this.renderButton("Register", " register-button")}
-                <hr />
-                <p>
-                  <span className="card-text">Already a member?</span>
-                  <a href="/login">Login!</a>
-                </p>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { success, message } = this.state.response;
+		return (
+			<div className="container appForm">
+				<div className="">
+					<div className="card-section border rounded p-3">
+						<div className="card-header-register pb-5">
+							<h2 className="card-header-title text-white pt-3">
+								Register
+							</h2>
+						</div>
+						<div className="card-body">
+							<form onSubmit={this.handleSubmit}>
+								{this.renderInput(
+									"username",
+									"Username",
+									"register-label"
+								)}
+								{this.renderInput(
+									"email",
+									"Email",
+									"register-label",
+									undefined,
+									success ? undefined : message
+								)}
+								{this.renderInput(
+									"password",
+									"Password",
+									"register-label",
+									"password"
+								)}
+								{this.renderButton(
+									"Register",
+									" register-button"
+								)}
+								<hr />
+								<p>
+									<span className="card-text">
+										Already a member?
+									</span>
+									<a href="/login">Login!</a>
+								</p>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default RegisterForm;

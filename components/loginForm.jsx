333276d99eb86ec7734aae33822889864362afe2
Dylan Lin <46723393+DylanLin29@users.noreply.github.com>
2020-07-22 18:17:34 -0700

    update

--- components/loginForm.jsx
+++ components/loginForm.jsx
@@ -5,72 +5,74 @@ import { Button } from "semantic-ui-react";
 const axios = require("axios");
 
 class LoginForm extends Form {
-  state = {
-    data: {
-      email: "",
-      password: "",
-    },
-    errors: {},
-    response: {
-      success: true,
-      message: "",
-    },
-  };
+	state = {
+		data: {
+			email: "",
+			password: "",
+		},
+		errors: {},
+		response: {
+			success: true,
+			message: "",
+		},
+	};
 
-  schema = {
-    email: Joi.string().required().label("Email"),
-    password: Joi.string().required().label("Password"),
-  };
+	schema = {
+		email: Joi.string().required().label("Email"),
+		password: Joi.string().required().label("Password"),
+	};
 
-  doSubmit = async () => {
-    // Call the server
-    try {
-      await axios.post("/api/auth", this.state.data);
-      Router.push("/");
-    } catch (err) {
-      if (err.response.status === 400) {
-        console.log(err.response.data);
-        this.setState({ response: err.response.data });
-      }
-    }
-  };
+	doSubmit = async () => {
+		// Call the server
+		try {
+			await axios.post("/api/auth", this.state.data);
+			Router.push("/");
+		} catch (err) {
+			if (err.response.status === 400) {
+				console.log(err.response.data);
+				this.setState({ response: err.response.data });
+			}
+		}
+	};
 
-  render() {
-    const { success, message } = this.state.response;
-    return (
-      <div className="container appForm">
-        <div className="card-section border rounded p-3">
-          <div className="card-header-login pb-5">
-            <h2 className="card-header-title text-white pt-3">Login</h2>
-          </div>
-          <div className="card-body">
-            <form onSubmit={this.handleSubmit}>
-              {!success && (
-                <div className="login-warning-wrapper">
-                  <Button negative className="login-warning">
-                    {message}
-                  </Button>
-                </div>
-              )}
-              {this.renderInput("email", "Email", "login-label")}
-              {this.renderInput(
-                "password",
-                "Password",
-                "login-label",
-                "password"
-              )}
-              {this.renderButton("Login", " login-button")}
-              <hr />
-              <p>
-                <span className="card-text">Not a member?</span>
-                <a href="/register">Sign up!</a>
-              </p>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { success, message } = this.state.response;
+		return (
+			<div className="container appForm">
+				<div className="card-section border rounded p-3">
+					<div className="card-header-login pb-5">
+						<h2 className="card-header-title text-white pt-3">
+							Login
+						</h2>
+					</div>
+					<div className="card-body">
+						<form onSubmit={this.handleSubmit}>
+							{!success && (
+								<div className="login-warning-wrapper">
+									<Button negative className="login-warning">
+										{message}
+									</Button>
+								</div>
+							)}
+							{this.renderInput("email", "Email", "login-label")}
+							{this.renderInput(
+								"password",
+								"Password",
+								"login-label",
+								"password"
+							)}
+							{this.renderButton("Login", " login-button")}
+							<hr />
+							<p>
+								<span className="card-text">Not a member?</span>
+								<a href="/register">Sign up!</a>
+							</p>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default LoginForm;
--- components/note.jsx
+++ components/note.jsx
@@ -2,59 +2,63 @@ import axios from "axios";
 import { Button } from "semantic-ui-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faTrash,
-  faClipboardCheck,
-  faSpinner,
-  faCheck,
+	faTrash,
+	faClipboardCheck,
+	faSpinner,
+	faCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 const Note = ({
-  title,
-  description,
-  importance,
-  category,
-  _id,
-  handleDelete,
+	title,
+	description,
+	importance,
+	category,
+	_id,
+	handleDelete,
 }) => {
-  const [check, setCheck] = useState(false);
+	const [check, setCheck] = useState(false);
 
-  const handleCheck = () => {
-    setCheck(true);
-  };
+	const handleCheck = () => {
+		setCheck(true);
+	};
 
-  const handleUnCheck = () => {
-    setCheck(false);
-  };
+	const handleUnCheck = () => {
+		setCheck(false);
+	};
 
-  return (
-    <div className="card-wrapper">
-      <div className="card">
-        <div className="card-img" />
-        <img src={`images/${category}.png`} className="profile-img" />
-        <p className="card-importance">{importance}</p>
-        {check && (
-          <FontAwesomeIcon className="card-check" icon={faCheck} size="2x" />
-        )}
-        <h1>{title}</h1>
-        <p className="card-date">Deadline</p>
-        <Button primary className="flip-button">
-          Flip
-        </Button>
-        <ul className="card-options">
-          <li onClick={() => handleDelete(_id)}>
-            <FontAwesomeIcon icon={faTrash} size="lg" />
-          </li>
-          <li onClick={() => handleCheck()}>
-            <FontAwesomeIcon icon={faClipboardCheck} size="lg" />
-          </li>
-          <li onClick={() => handleUnCheck()}>
-            <FontAwesomeIcon icon={faSpinner} size="lg" />
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="card-wrapper">
+			<div className="card">
+				<div className="card-img" />
+				<img src={`images/${category}.png`} className="profile-img" />
+				<p className="card-importance">{importance}</p>
+				{check && (
+					<FontAwesomeIcon
+						className="card-check"
+						icon={faCheck}
+						size="2x"
+					/>
+				)}
+				<h1>{title}</h1>
+				<p className="card-date">Deadline</p>
+				<Button primary className="flip-button">
+					Flip
+				</Button>
+				<ul className="card-options">
+					<li onClick={() => handleDelete(_id)}>
+						<FontAwesomeIcon icon={faTrash} size="lg" />
+					</li>
+					<li onClick={() => handleCheck()}>
+						<FontAwesomeIcon icon={faClipboardCheck} size="lg" />
+					</li>
+					<li onClick={() => handleUnCheck()}>
+						<FontAwesomeIcon icon={faSpinner} size="lg" />
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Note;
